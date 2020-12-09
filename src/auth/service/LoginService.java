7eab149137b07e7b368b68cee58c5a3c53cbf763
joojoo <joojoo@DESKTOP-JR5CPIF>
2020-12-09 16:55:40 +0900

    board login

---
+++ src/auth/service/LoginService.java
@@ -0,0 +1,28 @@
+package auth.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import jdbc.ConnectionProvider;
+import member.dao.MemberDao;
+import member.model.Member;
+
+public class LoginService {
+	private MemberDao memberDao = new MemberDao();
+	
+	public User login(String id, String pw) {
+		try (Connection con = ConnectionProvider.getConnection()){
+			Member member = memberDao.selectById(con, id);
+			if(member == null) {
+				throw new LoginFailException();
+			}
+			if(!member.matchPassword(pw)) {
+				throw new LoginFailException();
+			}
+			return new User(member.getId(), member.getName());
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+}
