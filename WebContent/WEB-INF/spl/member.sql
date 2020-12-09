e23e1d6c6f531c486a4ab85bfab35eb5ae541aba
joojoo <joojoo@DESKTOP-JR5CPIF>
2020-12-09 16:56:03 +0900

    board DB table

---
+++ WebContent/WEB-INF/spl/member.sql
@@ -0,0 +1,9 @@
+CREATE TABLE member (
+    memberid VARCHAR2(50) PRIMARY KEY,
+    name VARCHAR2(50) NOT NULL,
+    password VARCHAR2(10) NOT NULL,
+    regdate DATE NOT NULL
+);
+ 
+ 
+ SELECT * FROM member;
