d58194726a7cf33e84f1aaa9e24a13c3320e8802
joojoo <joojoo@DESKTOP-JR5CPIF>
2020-12-10 19:19:56 +0900

    article

---
+++ src/article/dao/ArticleContentDao.java
@@ -0,0 +1,29 @@
+package article.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import article.model.ArticleContent;
+import jdbc.JdbcUtil;
+
+public class ArticleContentDao {
+	public ArticleContent insert(Connection con, ArticleContent content) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO article_content (article_no, content) " + "VALUES (?, ?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setLong(1, content.getNumber());
+			pstmt.setString(2, content.getContent());
+			int insertedCount = pstmt.executeUpdate();
+			if (insertedCount > 0) {
+				return content;
+			} else {
+				return null;
+			}
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+
+	}
+}
